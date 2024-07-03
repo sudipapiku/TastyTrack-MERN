2e6442832ddc973c8bf682142496fdbf87ebb208
Sudipa Biswas <sudipapiku@gmail.com>
2024-07-04 05:06:46 +0530

    MongoDB URL

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();  
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://Admin:Admin123@cluster0.qvfzctu.mongodb.net/food-del').then(() => {
+    await mongoose.connect(process.env.MONGODB_URI).then(() => {
         console.log("DB Connected");
     });
 }
