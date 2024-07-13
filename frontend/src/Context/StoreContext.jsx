ed77990f750ec23715994cd055106fd3bb5aa270
Sudipa Biswas <95086287+sudipapiku@users.noreply.github.com>
2024-07-13 10:59:05 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
-    const url ='http://localhost:4000';
+    const url ="https://tastytrack-backend.onrender.com";
     const [token, setToken] = useState("");
     const [food_list, setFoodList] = useState([]);
     
